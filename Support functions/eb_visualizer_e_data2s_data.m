02d7b07215216efc0c670fe083844e23453f3edf
UMCU-RIBS <e.j.aarnoutse@umcutrecht.nl>
2018-05-28 08:16:51 +0200

    Add files via upload

---
+++ Support functions/eb_visualizer_e_data2s_data.m
@@ -0,0 +1,30 @@
+function s_data = eb_visualizer_e_data2s_data(vertices, els, edata, hemi)
+len     = size(vertices, 1);
+s_data  = zeros(len, 1);
+fade    = 10;
+
+nonanind = find(~isnan(edata));
+
+for el = nonanind';
+    b_z = abs(vertices(:,3)-els(el,3));
+    b_y = abs(vertices(:,2)-els(el,2));
+    if strcmp(hemi, 'l'), 
+        b_x = abs(vertices(:,1) - els(el,1));  
+    elseif strcmp(hemi, 'r'), 
+        b_x = abs(els(el,1) - vertices(:,1)); %put everything on left hemisphere
+    end
+    [~, im] = min(b_x.^2+b_z.^2+b_y.^2);
+    % s_data(im) = edata(el);
+
+    b_z = abs(vertices(:,3)-vertices(im,3));
+    b_y = abs(vertices(:,2)-vertices(im,2));
+    if strcmp(hemi, 'l'), 
+        b_x = abs(vertices(:,1) - vertices(im,1));  
+    elseif strcmp(hemi, 'r'), 
+        b_x = abs(vertices(im,1) - vertices(:,1)); %put everything on left hemisphere
+    end
+    d = edata(el) * exp((-(b_x.^2+b_z.^2+b_y.^2))/(fade)); %exponential fall off % electrode_weight is a measure: cor, power, lag or whatever else
+    % take out data(e)from d, and all(all~=0) = data(e)
+    s_data = s_data + d;
+end
+% s_data = round(s_data);
